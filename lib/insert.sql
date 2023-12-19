1ca67ad076f6e36c3a324382a549e0f400c4382f
Emmi Lindsey <emmilindsey@gmail.com>
2023-12-19 13:04:33 -0700

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "M", "brown", "happy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 56, "F", "brown", "happy",1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 5, "F", "brown", "happy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 5, "F", "brown", "happy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 5, "F", "brown", "happy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 5, "F", "brown", "happy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 5, "F", "brown", "happy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 5, "F", "brown", "happy", 1);
